bb5bc653fdedb88dfd2d9e5f8f664aa1a0d4a5d3
Toshitak <142971223+Toshitak@users.noreply.github.com>
2023-09-26 18:04:06 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Script is working from a different file")
+
+
+// input
+var username = prompt("Give me the username")
+// processing
+var welcomeMessage = "This script works!" + username;
+// output
+alert(welcomeMessage);
