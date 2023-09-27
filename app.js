6cfdf6bb06e4475fd5093c98f00787888ec2ed57
Toshitak <142971223+Toshitak@users.noreply.github.com>
2023-09-27 13:48:37 +0530

    feat: Add innerText in the outputDiv

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
 console.log(textInput)
 function clickHandler(){
-    console.log("clicked!");
-    console.log("input",textInput.value);
+    outputDiv.innerText = "Hello!" + textInput.value;
 };
 btnTranslate.addEventListener("click", clickHandler)
